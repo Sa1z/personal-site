da068ea469ff5d8ac5fac272a68a5041cad68339
Dennis Diaz <Diaz3737@gmail.com>
2021-12-29 15:54:08 -0500

    Button functionailty added.

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ const aboutBtn = document.getElementById('about-app').addEventListener('click', 
 const skillsBtn = document.getElementById('skills-app').addEventListener('click', skillsBtnFunc);
 const portfolioBtn = document.getElementById('portfolio-app').addEventListener('click', portfolioBtnFunc);
 const contactBtn = document.getElementById('contact-app').addEventListener('click', contactBtnFunc);
-
+const contentText = document.getElementById('content-text');
 
 
 function startBtnFunc(e) {
-    alert('test...');
+    contentText.innerHTML = 'I am a supporter of free and open source software. If you are not sure what that means and want to know more, here is a handy video:  <iframe class="dtVideo" width="560" height="315" src="https://www.youtube.com/embed/cU6H2m9XuQY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 }
 
 function aboutBtnFunc(e) {
-    alert('test...');
+    contentText.innerHTML = "Hi, I'm Dennis! And I am a self taught web developer.";
 }
 
 function skillsBtnFunc(e) {
-    alert('test...');
+    contentText.innerHTML = "<ul><li>HTML</li><li>CSS</li><li>Javascript</li><li>Python</li><li>Git</li><li>Currently learning React.</li></ul>";
 }
 
 function portfolioBtnFunc(e) {
-    alert('test...');
+    contentText.innerHTML = '<a href="https://github.com/Sa1z?tab=repositories" target="blank">My Github repositories</a>';
 }
 
 function contactBtnFunc(e) {
-    alert('test...');
+    contentText.innerHTML = "Contact";
 }
