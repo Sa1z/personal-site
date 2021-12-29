959c32075705c109bcd6dbaaeaaa76d83209b50b
Dennis Diaz <Diaz3737@gmail.com>
2021-12-28 21:51:02 -0500

    Initial javascript added.

---
+++ script.js
@@ -0,0 +1,27 @@
+const startBtn = document.getElementById('start-button').addEventListener('click', startBtnFunc);
+const aboutBtn = document.getElementById('about-app').addEventListener('click', aboutBtnFunc);
+const skillsBtn = document.getElementById('skills-app').addEventListener('click', skillsBtnFunc);
+const portfolioBtn = document.getElementById('portfolio-app').addEventListener('click', portfolioBtnFunc);
+const contactBtn = document.getElementById('contact-app').addEventListener('click', contactBtnFunc);
+
+
+
+function startBtnFunc(e) {
+    alert('test...');
+}
+
+function aboutBtnFunc(e) {
+    alert('test...');
+}
+
+function skillsBtnFunc(e) {
+    alert('test...');
+}
+
+function portfolioBtnFunc(e) {
+    alert('test...');
+}
+
+function contactBtnFunc(e) {
+    alert('test...');
+}
